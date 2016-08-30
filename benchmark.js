992519c7b3ffb76f5acaed5a95307eacf7892e8d
Ari Adiprana & Ryan Andhaka <pair+ariadiprana.ryanandhaka@hacktiv8.com>
2016-08-30 18:19:49 +0700

    binary-vs-linear

--- benchmark.js
+++ benchmark.js
@@ -6,19 +6,31 @@ var search = require('./binary_linear');
 var randomAngka = new Array()
 var awal = 0
 var akhir = 0
+var waktuLinear = 0;
+var waktuBinary = 0;
 
 //randomAngka
 awal = microtime.now();
-for (var i = 0; i < 10000; i++) {
-  randomAngka.push(getRandomInt(1,1000))
+for (var i = 0; i < 10000000; i++) {
+  randomAngka.push(i)
 }
 akhir = microtime.now();
 console.log("finishing waktu randomAngka : "+(akhir-awal)+" ms")
 
 awal = microtime.now();
-console.log(search.linearSearch(randomAngka,18 ));
+console.log(search.linearSearch(randomAngka,34));
 akhir = microtime.now();
 console.log("finishing waktu searchAngka linearSearch : "+(akhir-awal)+" ms")
+waktuLinear = akhir-awal
+
+
+awal = microtime.now();
+console.log(search.binarySearch(randomAngka,34));
+akhir = microtime.now();
+console.log("finishing waktu searchAngka BinarySearch : "+(akhir-awal)+" ms")
+waktuBinary = akhir-awal
+
+console.log("Perbedaan waktu linear & binary adalah "+(waktuLinear-waktuBinary)+" ms");
 
 // benchmark code here
 function getRandomInt(min, max) {
--- binary_linear.js
+++ binary_linear.js
@@ -15,6 +15,28 @@ search.linearSearch = function(values, target){
 
 search.binarySearch = function(values, target){
   //binary search code here
+  var awal = 0;
+  var akhir = values.length - 1;
+  var indeksSkrg;
+  var elementSkrg;
+
+  while (awal <= akhir) {
+      indeksSkrg = (awal + akhir) / 2 | 0;
+      elementSkrg = values[indeksSkrg];
+
+      if (elementSkrg < target) {
+          awal = indeksSkrg + 1;
+      }
+      else if (elementSkrg > target) {
+          akhir = indeksSkrg - 1;
+      }
+      else {
+          return indeksSkrg;
+          //return true;
+      }
+  }
+
+  return -1;
 }
 
 module.exports = search;
