2a26f9b9f0d9ec9ef7b1b1016737753ff841098b
Ari Adiprana & Ryan Andhaka <pair+ariadiprana.ryanandhaka@hacktiv8.com>
2016-08-30 17:35:25 +0700

    binary-vs-linear

--- benchmark.js
+++ benchmark.js
@@ -1,6 +1,28 @@
 "use strict";
 
 var Benchmark = require('benchmark');
+var microtime = require('microtime');
 var search = require('./binary_linear');
+var randomAngka = new Array()
+var awal = 0
+var akhir = 0
+
+//randomAngka
+awal = microtime.now();
+for (var i = 0; i < 10000; i++) {
+  randomAngka.push(getRandomInt(1,1000))
+}
+akhir = microtime.now();
+console.log("finishing waktu randomAngka : "+(akhir-awal)+" ms")
+
+awal = microtime.now();
+console.log(search.linearSearch(randomAngka,18 ));
+akhir = microtime.now();
+console.log("finishing waktu searchAngka linearSearch : "+(akhir-awal)+" ms")
 
 // benchmark code here
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
--- binary_linear.js
+++ binary_linear.js
@@ -2,10 +2,16 @@
 
 var search = {};
 
-
 search.linearSearch = function(values, target){
   //linear search code here
+  for (var i = 0; i < values.length; i++) {
+    if (target == values[i]) {
+      return i;
+    }
+  }
+  return -1;
 }
+
 
 search.binarySearch = function(values, target){
   //binary search code here
